d697318191d8a823f717d47921b1ac74033a052e
ISELL <i.ellis@nfi.nl>
2022-08-18 16:00:03 +0200

    BERT embeddings: remove main for allinone

--- bert_embeddings.py
+++ bert_embeddings.py
@@ -52,12 +52,12 @@ class BERTEmbeddings(MetaExtractionPlugin):
             log.info(str(embedding))
             trace.add_tracelet(to_tracelet(embedding))
 
-
+"""
 if __name__ == "__main__":
-    run_with_hanskenpy(BERTEmbeddings, endpoint='http://localhost:9091/gatekeeper/', 
+   run_with_hanskenpy(BERTEmbeddings, endpoint='http://localhost:9091/gatekeeper/', 
         # the keystore REST endpoint when this script was exported, note that
         # this can be overridden with --keystore
         keystore='http://localhost:9090/keystore/',
         # the project id of the project named "Semantic search",
         project='326c693c-45bc-4648-b8e3-24abb4f43c1d')
-       
+"""   
