6fecec5c1e84e1cb8739d7a70c408341557e3506
Bart Broere <b.broere@nfi.nl>
2022-08-29 14:45:19 +0200

    Fixes

--- hacky_inference_microservice.py
+++ hacky_inference_microservice.py
@@ -3,18 +3,20 @@ Spins up a Flask server that can do inference using the sentence transformers.
 
 Definitely not the final product, just for demo purposes
 """
-from flask import Flask
+from flask import Flask, request
+from sentence_transformers import SentenceTransformer
 from hansken.util import Vector
 
 app = Flask(__name__)
 models = {model_name: SentenceTransformer(model_name) 
           for model_name in ['all-MiniLM-L6-v2', 'clip-ViT-B-32']}
 
+
 @app.route("/")
 def inference():
-    model = request.args.get('model')
+    model = models[request.args.get('model')]
     sentence = request.args.get('sentence')
-    return str(Vector.from_sequence(model.encode(sentence)))
+    return str(Vector.from_sequence(model.encode(str(sentence))))
 
 
 if __name__ == '__main__':
